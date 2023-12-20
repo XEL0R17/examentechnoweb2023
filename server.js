42d097ae0f0e5a57e2f5ad984d808fb509bc5fe5
XS3L0R17 <195105@ecam.be>
2023-12-20 01:34:50 +0100

    addtvcasse

--- server.js
+++ server.js
@@ -21,6 +21,19 @@ app.post("/add", async function (req, res) {
 });
 
 
+app.post("/addTvCassee", async function (req, res) {
+  const tvCassee = new TvCassee({
+    marque: req.body.marque, // Assurez-vous que le formulaire contient un champ 'marque'
+    prix: req.body.prix, // Assurez-vous que le formulaire contient un champ 'prix'
+    taille: req.body.taille, // Assurez-vous que le formulaire contient un champ 'taille'
+    descriptionCasse: req.body.descriptionCasse // Assurez-vous que le formulaire contient un champ 'descriptionCasse'
+  });
+  
+  await tvCassee.save();
+  res.redirect('/'); // ou vers une page de confirmation ou de succès
+});
+
+
 app.get("/delete/:id", async function (req, res) {
   await Task.delete({ id: req.params.id });
   res.redirect('/');
@@ -31,8 +44,14 @@ app.get("/delete/:id", async function (req, res) {
 app.get("/", async function (req, res) {
   const televisions = await Television.loadMany(); // Récupérez les données de télévisions
   const achats = await Achat.loadMany();
-  const usees = await TvCassee.loadMany();
-  res.render('listTelevisions.ejs', { televisions: televisions,  achats: achats}); // Utilisez le modèle EJS pour les télévisions
+  const tvCassees = await TvCassee.loadMany(); // Récupérez les données de télévisions cassées
+
+  res.render('listTelevisions.ejs', {
+    televisions: televisions,
+    achats: achats,
+    tvCassees: tvCassees // Ajoutez les données de télévisions cassées à la vue
+  });
 });
+
 
 app.listen(4000);
