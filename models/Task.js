70cc4c88d7205e611f7c239817db43b1968ec092
XS3L0R17 <195105@ecam.be>
2023-12-20 10:02:00 +0100

    delete inutile

--- models/Task.js
+++
@@ -1,7 +0,0 @@
-import Model from './Model.js';
-
-export default class Task extends Model {
-
-  static table = ".tasks";
-  static primary = ["id"];
-}
--- models/Televisions.js
+++
@@ -1,7 +0,0 @@
-import Model from './Model.js';
-
-export default class Television extends Model {
-
-  static table = "henni.television";
-  static primary = ["id"];
-}
